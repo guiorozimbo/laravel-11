4e3e3d31d79591e195e6a17e11c348c7cebc6781
Guilherme <guilhermegv890@gmail.com>
2024-07-04 17:14:22 -0300

    Commit controller for users

--- app/Http/Requests/StoreUserRequest.php
+++ app/Http/Requests/StoreUserRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class StoreUserRequest extends FormRequest
 {
@@ -23,8 +24,7 @@ class StoreUserRequest extends FormRequest
     {
         return [
         'name' => 'required|string||min:3|max:255',
-        'email' => ['required','email','unique:users,email', 'min:6',
-        'max:20'],
+      'email' => Rule::unique('users','email')->ignore($this->user,'id'),// ['required','email','unique:users,email', 'min:6','max:20'],
         'password'=> ['required','min:6','max:20',]
         ];
 
--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -6,6 +6,7 @@
 
 <form action="{{route('users.update',$user->id)}}" method="POST">
     @csrf()
+   @method('put')
     <input type="text" name="name" placeholder="Nome" value="{{$user->name}}">
     <input type="email" name="email" placeholder="E-mail" value="{{$user->email}}">
     <input type="password" name="password" placeholder="Senha">
