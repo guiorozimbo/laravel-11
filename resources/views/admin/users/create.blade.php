cd5e0e711b5d07ef7cfcf3d47c75bb00378d84bb
Guilherme <guilhermegv890@gmail.com>
2024-07-04 19:12:24 -0300

    Commit edit forms for the repository

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -3,14 +3,11 @@
 @section('title','Criar novos usuários')
 <h1>Novo Usuário</h1>
 {{--@include('admin.includes.errors')--}}
-<x-alert/>
+
 
 <form action="{{route('users.store')}}" method="POST">
     @csrf()
-    <input type="text" name="name" placeholder="Nome" value="{{old('name')}}">
-    <input type="email" name="email" placeholder="E-mail" value="{{old('email')}}">
-    <input type="password" name="password" placeholder="Senha">
-    <button type="submit" style="background-color: red">Enviar</button>
+@include('admin.users.partials.form')
 </form>
 @endsection
 </body>
